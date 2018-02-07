07e197dc508d7549d94d3becb464956a57995e55
Szkolenie <Szkolenie@Blueform>
2018-02-07 11:01:31 +0100

    ada

--- index.php
+++ index.php
@@ -24,6 +24,13 @@ and open the template in the editor.
          <button type="submit" id="btn2">Obejrzyj ofertę</button>
         </div>
     <?php
+    $conn = mysqli_connect('wojakiewicz.pl', 'wojakiewic_baza', 'bidn2912', 'wojakiewic_baza?zeroDateTimeBehavior=convertToNull', '3306');
+    if (!$conn) {
+        die('Could not connect to MySQL: ' . mysqli_connect_error());
+    }
+    mysqli_query($conn, 'SET NAMES \'utf8\'');
+// TODO: insert your code here.
+    mysqli_close($conn);
         ?>
     </body>
 </html>
