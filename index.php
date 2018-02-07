59d3510d57f2e722d41a4a84a0cb05857e0cb823
Szkolenie <Szkolenie@Blueform-compaq>
2018-02-07 10:07:49 +0100

    zmiany w pliku

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        <button id="btn1" >Złóż zamówienie</button>
         <?php
         // put your code here
         ?>
